49f91de54d95abb1f4614b366741559e8b3a8044
Somesh Vemula <ratansomesh@gmail.com>
2023-11-27 23:50:40 -0500

    setup the reusable api client

---
+++ src/services/api-client.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.themoviedb.org/3/movie",
+  params: {
+    api_key: "cab4630aec2578cb97c8176e269db357",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
